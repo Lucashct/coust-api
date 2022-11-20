8cbc536ea5e208a623421818f61f922579275cf6
Lucas Torres <lucascms11@gmail.com>
2022-11-20 18:24:44 -0300

    [CHANGE] Configurando variáveis de ambiente

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { AppDataSource } from "./data-source"
 import * as morgan from 'morgan';
 import * as express from 'express';
 import * as cors from 'cors'
+import routes from "./router";
 
 AppDataSource.initialize().then(async () => {
     const app = express();
@@ -9,6 +10,7 @@ AppDataSource.initialize().then(async () => {
     app.use(express.json);
     app.use(morgan('dev'));
     app.use(cors());
-    
+    app.use(routes);
 
+    return app.listen(process.env.PORT, () => console.log(`Aplicação online na porta ${process.env.PORT}`));
 }).catch(error => console.log(error))
