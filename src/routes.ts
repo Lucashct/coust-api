6a54bc2fc1181265bd283c4d41e9e74d27a85d30
Lucas Torres <lucascms11@gmail.com>
2022-12-18 18:12:42 -0300

    [CHANGE] Alterando rota cartoes/listar para o método get.

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,6 @@ const routes = Router();
 
 //ROTAS CARTÕES
 routes.post('/cartao/gravar', new CartaoController().createCartao);
-routes.post('/cartao/listar', new CartaoController().listCartoes);
+routes.get('/cartao/listar', new CartaoController().listCartoes);
 
 export default routes;
